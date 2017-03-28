24a10883521923c11f2a6e19f2c63bb9b0c15267
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-03-28 16:33:12 -0700

    added user signup required input validation
    added server required field validation for signup page inputs

--- resource/database.php
+++ resource/database.php
@@ -3,7 +3,6 @@
     require_once('connect.php');
     try{
         $db = new PDO("{$dbConnectInfo['driver']}:host={$dbConnectInfo['host']};dbname={$dbConnectInfo['dbname']}", $dbConnectInfo['username'], $dbConnectInfo['password'], $dbConnectInfo['options']);
-        echo "Connected to the database";
     } catch (PDOException $exception) {
         echo "Connection failed: ".$exception->getMessage();
     } finally {
--- signup.php
+++ signup.php
@@ -2,31 +2,52 @@
 <?php
     include_once('resource/database.php');
 
-    if (isset($_POST['email'])){
-        $email = $_POST['email'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        require_once('./resource/password_encrypt.php');
-        $password = passwordEncrypt($password);
+    if (isset($_POST['signup'])){
+        $formErrors = [];
 
-        try {
-
-            $sqlInsert = "INSERT INTO `users` (`username`, `email`, `password`, `join_date`)
-                      VALUES (:username, :email, :password, now())";
-
-            $statement = $db->prepare($sqlInsert);
-            $statement->execute([
-                ':username' => $username,
-                ':email' => $email,
-                ':password' => $password
-            ]);
-
-            if ($statement->rowCount() > 0){
-                $result = '<p style="padding: 20px; color: green;">Registration Successful</p>';
+        $requiredFields = ['email', 'username', 'password'];
+        foreach($requiredFields as $requiredField){
+            if (empty($_POST[$requiredField])){
+                $formErrors[] = "$requiredField is a required field";
             }
+        }
 
-        } catch (PDOException $exception) {
-            $result = '<p style="padding: 20px; color: red;">An error occurred: '.$exception->getMessage().'</p>';
+        if (empty($formErrors)){
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            require_once('./resource/password_encrypt.php');
+            $password = passwordEncrypt($password);
+
+            try {
+
+                $sqlInsert = "INSERT INTO `users` (`username`, `email`, `password`, `join_date`) VALUES (:username, :email, :password, now())";
+
+                $statement = $db->prepare($sqlInsert);
+                $statement->execute([
+                    ':username' => $username,
+                    ':email' => $email,
+                    ':password' => $password
+                ]);
+
+                if ($statement->rowCount() > 0){
+                    $result = '<p style="padding: 20px; color: green;">Registration Successful</p>';
+                }
+
+            } catch (PDOException $exception) {
+                $result = '<p style="padding: 20px; color: red;">An error occurred: <ul style="color: red;"><li>'.$exception->getMessage().'</li></ul></p>';
+            }
+        } else {
+            if (count($formErrors) === 1){
+                $result = '<p style="color: red;">An error occurred: ';
+            } else {
+                $result = '<p style="color: red;">Some errors occurred: ';
+            }
+            $result .= '<ul style="color: red;">';
+            foreach ($formErrors as $formError) {
+                $result .= "<li>$formError</li>";
+            }
+            $result .= '</ul></p>';
         }
     }
 ?>
@@ -47,7 +68,7 @@
             <tr><td>Email:</td> <td><input type="email" value="" name="email"></td></tr>
             <tr><td>Username:</td> <td><input type="text" value="" name="username"></td></tr>
             <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-            <tr><td></td><td><input style="float: right;" type="submit" value="signup"</td></tr>
+            <tr><td></td><td><input style="float: right;" type="submit" name="signup"</td></tr>
         </table>
     </form>
 
